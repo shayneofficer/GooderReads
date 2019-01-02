766346d2df7718af8f58e81745fada05768a4dc4
stevelucascs <stevelucascs@gmail.com>
2019-01-02 11:52:31 -0600

    Basic Handlebars implementation

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // var apiRoutes = require("./routes/api-routes");
 // app.use(apiRoutes);
 
