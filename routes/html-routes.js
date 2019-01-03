a41bb98a1b539ad403e0dd79b224a64a60407c51
stevelucascs <stevelucascs@gmail.com>
2019-01-03 16:03:40 -0600

    Added dark mode, animations, and changed navbar styling

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,30 +7,7 @@ router.use(express.urlencoded({
 }));
 router.use(express.json());
 
-router.get('/basic-home', function (req, res) {
-  //Popular / Featured books
-  var placeholderImg = 'http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder-350x350.png';
-  var books = [{
-      title: 'Hunt for Red October',
-      cover: placeholderImg,
-      summary: 'This is a summary.'
-    },
-    {
-      title: 'Harry Pottah',
-      summary: 'This is a summary, with a British accent.',
-      cover: placeholderImg
-    },
-    {
-      title: 'Lord of the Rings',
-      summary: 'This is a boring summary.',
-      cover: placeholderImg,
-    }
-  ];
-  res.render('basic-home', {
-    books: books
-  });
-});
-
+// Landing page for users already signed in
 router.get('/user-home', function (req, res) {
   //Popular / Featured books
   var placeholderImg = 'http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder-350x350.png';
@@ -60,10 +37,29 @@ router.get('/register', function (req, res) {
   res.sendFile(path.join(__dirname, '../public/register.html'));
 });
 
-// A default, catch-all route that leads to index.html which displays the home page.
+// Default landing page
 router.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  //Popular / Featured books 
+  var placeholderImg = 'http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder-350x350.png';
+  var books = [{
+      title: 'Hunt for Red October',
+      cover: placeholderImg,
+      summary: 'This is a summary.'
+    },
+    {
+      title: 'Harry Pottah',
+      summary: 'This is a summary, with a British accent.',
+      cover: placeholderImg
+    },
+    {
+      title: 'Lord of the Rings',
+      summary: 'This is a boring summary.',
+      cover: placeholderImg,
+    }
+  ];
+  res.render('basic-home', {
+    books: books
+  });
 });
-
 
 module.exports = router;
