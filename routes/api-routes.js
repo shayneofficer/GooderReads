6669420b5256675ae8aeba0a261b281ab62a35e1
Nate <nathanw88@gmail.com>
2019-01-03 16:12:57 -0600

    minor var changes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,59 +3,60 @@ var express = require("express");
 var path = require("path");
 var router = express.Router();
 var books = require('google-books-search')
+var goodreads = require('goodreads-api-node');
 
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 
 router.post("/api/registerUser", function (req, res) {
-    user.selectWhere("userName", req.body.userName,
-        function (result) {
-            if (result && result.length) {
-                console.log(`User "${result[0].userName}" already exists`);
-                console.log(result);
-            } else {
-                console.log("Create");
-                user.create(
-                    ["userName", "userPassword", "userEmail"],
-                    [req.body.userName, req.body.userPassword, req.body.userEmail],
-                    function (result) {
-                        // Send back the ID of the new quote
-                        res.json({ id: result.insertId });
-                    }
-                );
-            }
-        }
-    );
+  user.selectWhere("userName", req.body.userName,
+    function (result) {
+      if (result && result.length) {
+        console.log(`User "${result[0].userName}" already exists`);
+        console.log(result);
+      } else {
+        console.log("Create");
+        user.create(
+          ["userName", "userPassword", "userEmail"],
+          [req.body.userName, req.body.userPassword, req.body.userEmail],
+          function (result) {
+            // Send back the ID of the new quote
+            res.json({ id: result.insertId });
+          }
+        );
+      }
+    }
+  );
 });
 
 function getBooks(title, cb) {
-    books.search(title, function (error, results) {
-        if (!error) {
-            var books = []
-            for (var i = 0; i < results.length; i++) {
-                var bookInfo = results[i];
-                var identifiers = []
-                for (var j = 0; j < bookInfo.industryIdentifiers.length; j++) {
-                    identifiers.push({ type: bookInfo.industryIdentifiers[j].type, identifier: bookInfo.industryIdentifiers[j].identifier })
-                }
-                books.push({
-                    title: bookInfo.title,
-                    author: bookInfo.authors,
-                    publisher: bookInfo.publisher,
-                    publishedDate: bookInfo.publishedDate,
-                    description: bookInfo.description,
-                    image: bookInfo.thumbnail,
-                    categories: bookInfo.categories,
-                    pageCount: bookInfo.pageCount,
-                    identifier: identifiers
-                });
-            }
-            cb(books)
+  books.search(title, function (error, results) {
+    if (!error) {
+      var booksArr = []
+      for (var i = 0; i < results.length; i++) {
+        var bookInfo = results[i];
+        var identifiers = []
+        for (var j = 0; j < bookInfo.industryIdentifiers.length; j++) {
+          identifiers.push({ type: bookInfo.industryIdentifiers[j].type, identifier: bookInfo.industryIdentifiers[j].identifier })
         }
-        else {
-            console.log(error);
-        }
-    });
+        booksArr.push({
+          title: bookInfo.title,
+          author: bookInfo.authors,
+          publisher: bookInfo.publisher,
+          publishedDate: bookInfo.publishedDate,
+          description: bookInfo.description,
+          image: bookInfo.thumbnail,
+          categories: bookInfo.categories,
+          pageCount: bookInfo.pageCount,
+          identifier: identifiers
+        });
+      }
+      cb(booksArr)
+    }
+    else {
+      console.log(error);
+    }
+  });
 }
 
 module.exports = router;
