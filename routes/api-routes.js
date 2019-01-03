5e2cbb31ca9539f2e9b2904e88bd81479433f3c7
Nate <nathanw88@gmail.com>
2019-01-03 11:08:53 -0600

    added google book search node module

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,7 @@ var user = require("../models/users");
 var express = require("express");
 var path = require("path");
 var router = express.Router();
+var books = require('google-books-search')
 
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
@@ -16,5 +17,40 @@ router.post("/api/registerUser", function (req, res) {
         }
     );
 });
+
+
+function getBooks(title, ISBN, cb) {
+books.search(title, function(error, results) {
+    if ( ! error ) {
+      
+        
+      for (var i = 0; i < results.length; i++){
+        var bookInfo = results[i];
+        if (bookInfo.hasOwnProperty("industryIdentifiers")){
+          for(var j = 0; j < bookInfo.industryIdentifiers.length; j++){
+            if (bookInfo.industryIdentifiers[j].identifier === ISBN){
+              var book ={
+                description: bookInfo.description,
+                image: bookInfo.thumbnail,
+                categories: bookInfo.categories,
+                pageCount: bookInfo.pageCount,
+                ISBN: bookInfo.industryIdentifiers[j].identifier
+              };
+              console.log(book)
+        //   cb(book)
+              return
+            }
+          }
+        }
+        
+    }
+}
+    else {
+        console.log(error);
+    }
+});
+
+}
+
 
 module.exports = router;
