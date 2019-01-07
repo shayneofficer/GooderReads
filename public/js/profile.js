e8ed4201681439a992f1d69bffbcdd81cd4f0322
stevelucascs <stevelucascs@gmail.com>
2019-01-07 15:46:52 -0600

    validated

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,43 @@
-if (sessionStorage.getItem('userID') === -4) {
-  window.location.replace('/home')
-} else {
-  var user = sessionStorage.getItem('userID')
-  $.get('/profile/' + user).then(function () {})
+window.onload = new function (event) {
+  console.log('on load')
+  console.log(window.location.pathname)
+  if (window.location.pathname == '/profile' || window.location.pathname == '/genres')
+  {
+    console.log('1')
+    if (sessionStorage.getItem('userID') < 0) {
+      // User is NOT logged in
+      var userID
+      $('#modalLoginForm').modal('show')
+      $('#loginFormSubmit').on('click', function (event) {
+        validateLogin(function () {
+          userID = sessionStorage.getItem('userID')
+          console.log(userID)
+        })
+      })
+      $('#registerFormSubmit').on('click', function (event) {
+        validateRegister(function () {
+          userID = sessionStorage.getItem('userID')
+          console.log(userID)
+        })
+      })
+      $('#loginFormClose').on('click', function (event) {
+        event.preventDefault()
+        userID = sessionStorage.getItem('userID')
+        if (userID < 0) {
+          window.location.replace('/home')
+        }
+      })
+      $('#registerFormClose').on('click', function (event) {
+        event.preventDefault()
+        userID = sessionStorage.getItem('userID')
+        if (userID < 0) {
+          window.location.replace('/home')
+        }
+      })
+    } else {
+      // User is already logged in
+      window.location.replace('/profile/' + userID)
+    }
+  }
 }
+
