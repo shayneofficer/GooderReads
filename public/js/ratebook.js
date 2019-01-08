61610b7b08ec0eb40390bcbf0aaa69281e67190c
stevelucascs <stevelucascs@gmail.com>
2019-01-07 18:14:13 -0600

    stars were born

--- public/js/ratebook.js
+++ public/js/ratebook.js
@@ -1,34 +1,75 @@
-$(".rate-book").on("click", function (event) {
-  event.preventDefault();
-  rating = { isbn: $(this).data("isbn"), rating: parseInt($(this).text()), userID: sessionStorage.getItem("userID") }
-
-  console.log(rating);
-  $.post("/api/rate-book/", rating, function (data) {
-    if (data.error) {
-      alert(data.error);
-    } else {
-      location.reload();
+/**
+ * Star hover event; highlights stars corresponding to rating
+ */
+var parent
+$('.star')
+  .mouseenter(function () {
+    parent = $(this).parent()
+    for (var i = 0; i < $(this).data('val'); i++) {
+      var star = $(parent).children()[i]
+      $(star).css('color', '#DE681C')
     }
-  });
-});
+  })
+  .mouseleave(function () {
+    for (var i = 0; i < $(this).data('val'); i++) {
+      var star = $(parent).children()[i]
+      $(star).css('color', '#AAB5C1')
+    }
+  })
 
-if (sessionStorage.getItem("userName") == "" || sessionStorage.getItem("userID") < 0) {
-  console.log("hide dropdown");
-  $(".ratings-dropdown").hide();
-} else {
-  $(".ratings-dropdown").show();
-}
+/**
+ * Load the user's saved ratings if they have any
+ */
+// $.get(
+//   '/api/book-rating/' +
+//     $(this).data('isbn') +
+//     '/' +
+//     sessionStorage.getItem('userID'),
+//   function (data) {
+//     if (data) {
+//       console.log(data)
+//       if ($(this).data('isbn') == Object.values(data[0])[1]) {
+//         var userRating = Object.values(data[0])[2]
+//         console.log(userRating)
+//         // $(this).html('Your Rating: ' + Object.values(data[0])[2])
+//       }
+//     }
+//   }
+// )
 
-$(".ratings-dropdown").each(function () {
-  if (!sessionStorage.getItem("userName") == "" && sessionStorage.getItem("userID") >= 0) {
-    $.get("/api/book-rating/" + $(this).data("isbn") + "/" + sessionStorage.getItem("userID"), function (data) {
-      if (data) {
-        $(".ratings-dropdown").each(function () {
-          if ($(this).data("isbn") == Object.values(data[0])[1]) {
-            $(this).html("Your Rating: " + Object.values(data[0])[2]);
-          }
-        });
+/**
+ * Star on click event (posts to server)
+ */
+$('.star').on('click', function (event) {
+  event.preventDefault()
+
+  if (sessionStorage.getItem('userID') < 0) {
+    // Prompt user to log in if they aren't already
+    $('#modalLoginForm').modal('show')
+  } else {
+    // keep stars gold when submitted
+    var parent = $(this).parent()
+    var children = $(parent).children()
+    for (var i = 0; i < $(children).length; i++) {
+      if ($(children[i]).hasClass('saved-rating')) {
+        $(children[i]).removeClass('saved-rating')
       }
-    });
+    }
+    for (var i = 0; i < $(this).data('val'); i++) {
+      var star = $(parent).children()[i]
+      $(star).addClass('saved-rating');
+    }
+    // Value of the star clicked
+    rating = {
+      isbn: $(this).data('isbn'),
+      rating: $(this).data('val'),
+      userID: sessionStorage.getItem('userID')
+    }
+
+    $.post('/api/rate-book/', rating, function (data) {
+      if (data.error) {
+        alert(data.error)
+      }
+    })
   }
-});
+})
